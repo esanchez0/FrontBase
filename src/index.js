a77abd2d6a62d6e29444bb441b44b5ca44235159
Eder Sanchez <eder_una_df@hotmail.com>
2023-11-30 19:14:42 -0600

    Se inicializa proyecto base Recat 17 Mui 5

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// Librerias para usar el ContextApi(variables globales)
+// import { initialState } from "./contexto/initialState"; // variable contenedora de todas las variables
+// import { StateProvider } from "./contexto/store"; //almacena variables globales, suscribe a todos los componentes
+// import { mainReducer } from "./contexto/reducers"; //indice de los reducers, todas las variables globales, quien tiene acceso a todas esas variables
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    {/* <StateProvider initialState={initialState} reducer={mainReducer}> */}
+      <App />
+    {/* </StateProvider> */}
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
