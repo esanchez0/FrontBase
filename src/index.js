74365b8ccd20f79cab280d423fbdef231bf06f15
Eder Sanchez <eder_una_df@hotmail.com>
2023-12-04 22:46:52 -0600

    Se sube version react 18

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./App";
+import ReactDOM from "react-dom/client";
 
 // Librerias para usar el ContextApi(variables globales)
 import { initialState } from "./contexto/initialState"; // variable contenedora de todas las variables
 import { StateProvider } from "./contexto/store"; //almacena variables globales, suscribe a todos los componentes
 import { mainReducer } from "./contexto/reducers"; //indice de los reducers, todas las variables globales, quien tiene acceso a todas esas variables
 
-ReactDOM.render(
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+ root.render(
   <React.StrictMode>
     <StateProvider initialState={initialState} reducer={mainReducer}>
       <App />
     </StateProvider>
   </React.StrictMode>,
-  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
